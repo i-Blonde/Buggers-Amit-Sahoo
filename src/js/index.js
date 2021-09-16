935e7b6c3a26d971fcd030b6777e4585bfb06b51
amitlvs <amit007.mail2@gmail.com>
2021-09-17 00:46:02 +0530

    save

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,66 @@
 
- console.log("cc");
- function numberValidation(){
-     let num = document.querySelector(".num").value;
-     if(num.length == 10 || num == ""){
-        document.querySelector(".error").innerHTML = ` `;
-     }
-    // else if(num.length <10){
-    //     document.querySelector(".error").innerHTML = `**Please enter a correct number**`;
-    // }else if (num >= 'a' && num <= 'z'){
-    //     document.querySelector(".error").innerHTML = "****"
-    // }
-}
+// Contact Number Validation
+    function numValid() {
+        let num = document.querySelector("#num").value;
+        let pnum = parseInt(num);
+        // console.log(num);
+        var inp = /[^0-9]/gi;
+        // console.log(inp);
+        if(pnum.length < 10 && num == " " ){
+           
+            document.querySelector("#num").style.borderColor = "Red";
+        }if(num == ""){
+            document.querySelector("#num").style.borderColor = "";
+        }
+        if(num.length == 10){
+            document.querySelector("#num").style.borderColor = "Green";
+        }
+    }
 
-function emailValidation() {
-    let mail = document.querySelector(".mail").value;
-    if(mail == null){
-        mail.style.border = "red";
-    }
-    else if(mail == ""){
-        document.querySelector(".error").innerHTML = ` `;
+//  function numberValidation(){
+//      let num = document.querySelector(".num").value;
+//      if(num.length == 10 || num == ""){
+//         // document.querySelector(".error").innerHTML = ` `;
+//         document.querySelector(".num").style.borderColor = "Green";
+//     }else if(num == "a"){
+//        document.querySelector(".num").style.borderColor = "Red";
+//     }
+//      else if(num.length <10){
+//         document.querySelector(".num").style.borderColor = "Red";
+//      }
+//     // else if(num.length <10){
+//     //     document.querySelector(".error").innerHTML = `**Please enter a correct number**`;
+//     // }else if (num >= 'a' && num <= 'z'){
+//     //     document.querySelector(".error").innerHTML = "****"
+//     // }
+// }
 
-    }
-    // if(mail == ""){
-    //     document.querySelector('.error').innerHTML = " **Please fill the email**";
-    // }
-    else if(mail.indexOf('@') <= 0){
-        document.querySelector('.error').innerHTML = " **Please enter the correct email**";
-        mail.style.border = "red";
-    }
-    else{
-        return false;
-    }
-}
+// function emailValidation() {
+//     let mail = document.querySelector(".mail").value;
+//     if(mail == null){
+//         mail.style.borderColor = "Red";
+//     }
+//     else if( mail.length -2 == "." || mail.length -3 == "."){
+//         document.querySelector(".error").innerHTML = ` `;
+//         document.querySelector(".mail").style.borderColor = "Green"
+
+//     }
+//     // if(mail == ""){
+//     //     document.querySelector('.error').innerHTML = " **Please fill the email**";
+//     // }
+//     // else if(mail.indexOf('@') <= 0){
+//     //     document.querySelector('.error').innerHTML = " **Please enter the correct email**";
+//     //     mail.style.border = "red";
+//     // }
+//     // else if(mail.length <= 0 || mail == " " || mail.length -2 != "." || mail.length -3 != "."){
+//     //     document.querySelector(".mail").style.borderColor = "Red";
+//     // }
+//     else{
+//         return false;
+//     }
+// }
+
+// Password Validation
 function passValidation() {
     let pass = document.querySelector(".pass").value;
     let st = "@#$!%^&*()_+><?:'{}[]\|";
@@ -41,6 +70,7 @@ console.log("@@");
     }
 }
 
+// Name Validation
 function nameValidation() {
     let name = document.querySelector(".name").value;
 
@@ -70,3 +100,12 @@ eye.addEventListener('mouseup', ()=> {
     inputPass.type = "password";
         
 })
+
+function emailValidation(){
+    let email = document.getElementsByClassName("mail");
+    if(email.match("a")){
+
+        console.log(email);
+    }
+}
+
